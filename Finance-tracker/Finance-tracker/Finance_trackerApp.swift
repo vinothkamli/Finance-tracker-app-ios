61d898617f43475808ba4ba54b777a4d7c03d593
vinoth_technotackle <vinod.kambil@technotackle.com>
2024-12-09 11:33:43 +0530

    Project-Initial-setup

---
+++ Finance-tracker/Finance-tracker/Finance_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_trackerApp.swift
+//  Finance-tracker
+//
+//  Created by TechnoTackle on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Finance_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
